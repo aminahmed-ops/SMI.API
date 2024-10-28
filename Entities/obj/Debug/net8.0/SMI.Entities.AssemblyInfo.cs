91c62d0c96624a7d017c8d4d021a97737f4e68cd
Amin Ahmed <73785301+aminahmed-ops@users.noreply.github.com>
2024-10-29 00:28:36 +0500

    changes

--- Entities/obj/Debug/net8.0/SMI.Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/SMI.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7c424134ac5a8c34491b49221c1283467493a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FacebookClient/obj/Debug/net8.0/FacebookClient.AssemblyInfo.cs
+++ FacebookClient/obj/Debug/net8.0/FacebookClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacebookClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7c424134ac5a8c34491b49221c1283467493a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacebookClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacebookClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
